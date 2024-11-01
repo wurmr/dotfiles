55f9db4932ee5e06a392cc642ea9dfe962743246
Jim Karg <wurmr@users.noreply.github.com>
2024-11-01 09:37:39 -0500

    feat: add hcl to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "typescript", "javascript", "json", "css" },
+    ensure_installed = { "typescript", "javascript", "json", "css", "hcl" },
   },
 }
