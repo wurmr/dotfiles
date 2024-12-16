f2afc0469bf15dc98f571683b7da4f4bcdecb19b
Jim Karg <wurmr@users.noreply.github.com>
2024-12-16 10:57:30 -0600

    feat: remove telescope references to nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>sx",
+--   require("telescope.builtin").resume,
+--   { noremap = true, silent = true, desc = "Resume" }
+-- )
 
 -- Map ; to command palet like NvChad
 vim.keymap.set("n", ";", ":", { noremap = true, silent = false })
 
 vim.keymap.set("n", "<leader>bs", ":SudaWrite<CR>", { desc = "Write with sudo" })
+
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
+
+-- vim.keymap.set("n", "yc", "yygccp", { noremap = true, desc = "Yank and comment" })
+-- vim.keymap.set("n", "<C-c>", "ciw")
--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -40,8 +40,8 @@ return {
     end,
   },
 
-  -- change some telescope options and a keymap to browse plugin files
   {
+    -- change some telescope options and a keymap to browse plugin files
     "nvim-telescope/telescope.nvim",
     keys = {
       -- add a keymap to browse plugin files
