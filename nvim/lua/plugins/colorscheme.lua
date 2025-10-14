b6b3facb037e3beddd57d54ebf57458247bec358
Jim Karg <wurmr@users.noreply.github.com>
2025-10-14 10:10:13 -0500

    feat(nvim): :sparkles: set nvim colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
