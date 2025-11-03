c095ceaec9229d6cbe9e6f95bd48a93e6b86a955
Jim Karg <wurmr@users.noreply.github.com>
2025-11-02 21:50:36 -0600

    feat(nvim): :sparkles: add keymap to reload file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,5 +12,7 @@ vim.keymap.set("n", "<leader>bs", ":SudaWrite<CR>", { desc = "Write with sudo" }
 
 vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
 
+vim.keymap.set("n", "<leader>bu", ":e!<CR>", { desc = "Undo (Reload from disk)" })
+
 -- vim.keymap.set("n", "yc", "yygccp", { noremap = true, desc = "Yank and comment" })
 -- vim.keymap.set("n", "<C-c>", "ciw")
