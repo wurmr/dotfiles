547f008c393af110b8716725ecf7a5613ad553c3
Jim Karg <wurmr@users.noreply.github.com>
2025-11-26 11:17:31 -0600

    feat(nvim): :sparkles: move to snacks.explorer

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,4 +1,5 @@
 return {
+  enabled = false,
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,7 +5,26 @@ return {
       ignored = true,
       hidden = true,
       sources = {
-        explorer = { hidden = true, ignored = false },
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = { ".git", "node_modules" },
+          layout = {
+            layout = { position = "right" },
+          },
+          win = {
+            input = {
+              keys = {
+                ["<esc>"] = { "", mode = "n" },
+              },
+            },
+            list = {
+              keys = {
+                ["<esc>"] = { "", mode = "n" },
+              },
+            },
+          },
+        },
         files = { hidden = true },
       },
     },
